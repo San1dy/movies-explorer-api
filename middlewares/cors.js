ee1e610e7fb2e3ace9028ff2f97bdd39849bf215
Ivan Alekseev <obivan266@gmail.com>
2023-08-18 18:21:36 +0300

    исправлен код

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -3,18 +3,19 @@ const allowedCors = [
   'http://san1dy.nomoredomains.xyz',
   'https://localhost:3000/',
   'http://localhost:3000/',
+  'localhost:3000',
+  'san1dy.nomoredomains.xyz',
 ];
-
-const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 
 module.exports = (req, res, next) => {
   const { origin } = req.headers;
   const { method } = req;
 
   const requestHeaders = req.headers['access-control-request-headers'];
-
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', true);
   }
 
   if (method === 'OPTIONS') {
