31419ae498c5fbc67c15e45cf4a8c9d8b9593113
Ivan Alekseev <obivan266@gmail.com>
2023-08-20 20:26:47 +0300

    изменения удалены

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -14,7 +14,7 @@ module.exports = (req, res, next) => {
   const requestHeaders = req.headers['access-control-request-headers'];
 
   if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Origin', origin);
   }
 
   if (method === 'OPTIONS') {
