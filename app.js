fcfd8e1e7a516a3dd7abca5bce9669bf0320d329
Ivan Alekseev <obivan266@gmail.com>
2023-08-28 21:19:04 +0300

    внесены правки

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ const helmet = require('helmet');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const { errors } = require('celebrate');
-const cors = require('cors');
+
 const limiter = require('./utils/limiter');
 
 const errorHandler = require('./middlewares/errorHandler');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
-
+const cors = require('./middlewares/cors');
 
 const router = require('./routes');
 
@@ -24,14 +24,7 @@ app.use(limiter);
 app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-app.use(cors({
-  origin: ['http://localhost:3000','http://san1dy.nomoredomains.xyz','https://san1dy.nomoredomains.xyz'],
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true,
-  allowedHeaders: 'Content-Type,Authorization' // Добавьте дополнительные заголовки сюда
-}));
-
+app.use(cors);
 
 app.use(router);
 
